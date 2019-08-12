1b264f6cc55f9009f1bac92a52e4258f76cb6f24
Double0101 <double0101@foxmail.com>
2019-08-12 21:08:56 +0800

    messages decode&encode

---
+++ rc-common/src/main/java/com/remote2call/common/handler/RcDecoder.java
@@ -0,0 +1,35 @@
+package com.remote2call.common.handler;
+
+import com.remote2call.common.util.SerializationUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class RcDecoder extends ByteToMessageDecoder {
+
+    private Class<?> genericClass;
+
+    public RcDecoder(Class<?> cls) {
+        this.genericClass = cls;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        if (byteBuf.readableBytes() < 4) {
+            return;
+        }
+        byteBuf.markReaderIndex();
+        int dataLength = byteBuf.readInt();
+        if (byteBuf.readableBytes() < dataLength) {
+            byteBuf.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        byteBuf.readBytes(data);
+
+        Object obj = SerializationUtil.deserialize(data, genericClass);
+        list.add(obj);
+    }
+}
---
+++ rc-common/src/main/java/com/remote2call/common/handler/RcEncoder.java
@@ -0,0 +1,24 @@
+package com.remote2call.common.handler;
+
+import com.remote2call.common.util.SerializationUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class RcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+
+    public RcEncoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Object o, ByteBuf byteBuf) throws Exception {
+        if (genericClass.isInstance(o)) {
+            byte[] data = SerializationUtil.serialize(o);
+            byteBuf.writeInt(data.length);
+            byteBuf.writeBytes(data);
+        }
+    }
+}
