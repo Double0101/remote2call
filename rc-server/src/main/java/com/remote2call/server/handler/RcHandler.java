72c64ccb78fcc9add584cbcded01538b9bcc5566
Double <double0101@foxmail.com>
2019-08-12 18:17:00 +0800

    go home

---
+++ rc-server/src/main/java/com/remote2call/server/handler/RcHandler.java
@@ -0,0 +1,54 @@
+package com.remote2call.server.handler;
+
+import com.remote2call.common.handler.BaseInboundHandler;
+import com.remote2call.common.net.RcRequest;
+import com.remote2call.common.net.RcResponse;
+import com.remote2call.server.RcServer;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class RcHandler extends BaseInboundHandler<RcRequest> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RcHandler.class);
+
+    private final Map<String, Object> handlerMap;
+
+    public RcHandler(Map<String, Object> handlerMap) {
+        this.handlerMap = handlerMap;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, RcRequest msg) throws Exception {
+        RcServer.submit(new Runnable() {
+            @Override
+            public void run() {
+                logger.debug("Receive request " + msg.getRequestId());
+                RcResponse response = new RcResponse();
+                response.setRequestId(msg.getRequestId());
+                try {
+                    Object result = handle(msg);
+                    response.setResult(result);
+                } catch (Throwable t) {
+                    response.setError(t.toString());
+                    logger.error("RcServer handle request error", t);
+                }
+
+                ctx.writeAndFlush(response).addListener(new ChannelFutureListener() {
+                    @Override
+                    public void operationComplete(ChannelFuture future) throws Exception {
+                        logger.debug("Send response for request " + msg.getRequestId());
+                    }
+                });
+            }
+        });
+    }
+
+    private Object handle(RcRequest request) throws Throwable {
+        return null;
+    }
+}
